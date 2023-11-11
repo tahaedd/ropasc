45f5acf9c5bd26aa6c1201348122679b4ee3f893
tahaedd <tahaedderkaoui@gmail.com>
2023-11-11 12:24:45 +0000

    final product without GUI

--- main.js
+++ main.js
@@ -2,16 +2,40 @@ let value = " ";
 let computerchoice = " ";
 let ys = 0;
 let cs = 0;
+const randerUser= document.getElementById('playerscore')
+const randerPc= document.getElementById('pcscore')
+const userchoice= document.getElementById('user')
+const pcchoice= document.getElementById('pc')
+const ifpc= document.getElementById('pcwin')
+const ifuser= document.getElementById('userwin')
+const stat= document.getElementById('stat')
 const choices = ['rock', 'paper', 'scissors'];
 
+
 const submain = (choice) => {
-    document.getElementById(choice).onclick = ()=> {
-        value = choice;
-        computerchoice = choices[Math.floor(Math.random()* 3)]
-        compare(value, computerchoice)
-        console.log(value,computerchoice)}
+    
+
+        document.getElementById(choice).onclick = ()=> {
+            
+            value = choice;
+            computerchoice = choices[Math.floor(Math.random()* 3)]
+            compare(value, computerchoice)
+            console.log(value,computerchoice)
+            pcchoice.innerHTML = computerchoice
+            userchoice.innerHTML = value
+            randerUser.innerHTML = ys
+            randerPc.innerHTML = cs
+            if(cs === 5 ){
+                stat.innerHTML = "You lose! HAHAHAHAHAHAHAHAHAHAHAHAHA"
+                document.getElementById(choice).onclick = ()=> {}
+            }else if(ys === 5){
+                stat.innerHTML = "You win, GO FUCK YOUR SELF WITH AN UMBRELLA"
+                document.getElementById(choice).onclick = ()=> {}
+            }
+        }      
 }
 const compare = (a,b) => {
+
     if (a === b){
         console.log("it's a draw")
     }
@@ -22,11 +46,10 @@ const compare = (a,b) => {
     else if(a === "scissors" && b === "paper"){ys++}
     else if(a === "scissors" && b === "rock"){cs++}
     console.log(`ys: ${ys}, cs: ${cs}`)
-
+        
 }
 
 submain(choices[0])
 submain(choices[1])
 submain(choices[2])
 
-    
