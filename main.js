6f08a26c34fa7fba5547e9a00c247ba0e74ce9be
tahaedd <tahaedderkaoui@gmail.com>
2023-11-11 10:28:57 +0000

    new modif

--- main.js
+++ main.js
@@ -3,28 +3,14 @@ let computerchoice = " ";
 let ys = 0;
 let cs = 0;
 const choices = ['rock', 'paper', 'scissors'];
-document.getElementById('rock').onclick = ()=> {
-    value = 'rock';
-    computerchoice = choices[Math.floor(Math.random()* 3)]
-    compare(value, computerchoice)
-    console.log(value,computerchoice)
-};
-document.getElementById('paper').onclick = ()=> {
-    value = 'paper';
 
-    computerchoice = choices[Math.floor(Math.random()* 3)]
-    compare(value, computerchoice)
-    console.log(value,computerchoice)
-
-};
-
-document.getElementById('scissors').onclick = ()=> {
-    value = 'scissors';
-    computerchoice = choices[Math.floor(Math.random()* 3)]
-    compare(value, computerchoice)
-    console.log(value,computerchoice)
-    
-};
+const submain = (choice) => {
+    document.getElementById(choice).onclick = ()=> {
+        value = choice;
+        computerchoice = choices[Math.floor(Math.random()* 3)]
+        compare(value, computerchoice)
+        console.log(value,computerchoice)}
+}
 const compare = (a,b) => {
     if (a === b){
         console.log("it's a draw")
@@ -39,6 +25,8 @@ const compare = (a,b) => {
 
 }
 
-
+submain(choices[0])
+submain(choices[1])
+submain(choices[2])
 
     
